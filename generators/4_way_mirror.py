67d2457dc0fe3c8e0050eaa9cf2344c688f2d37c
Liam <something.random.i.made.at.3am@gmail.com>
2023-02-27 14:05:49 +1300

    Bug fixes

--- generators/4_way_mirror.py
+++ generators/4_way_mirror.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageOps
+from PIL import Image
 import sys
 import random
 
@@ -29,11 +29,18 @@ for x in range(img.size[0]):
         if ascii_name[(x+y)%l] % 3 == 0:
             data[x,y] = (colour[0], colour[1], colour[2])
 
+# for x in range(img.size[0]):
+#     for y in range(img.size[1]):
+#         for z in range(1, size):
+#             print(z, size)
+#             data[size - z, y -1] = data[z - 1, y-1]
+
 for x in range(img.size[0]):
     for y in range(img.size[1]):
         for z in range(1, size):
             print(z, size)
-            data[size - z, x -1] = data[z - 1, x-1]
+            data[size - z, y -1] = data[z - 1, y-1]
+            data[y-1, size - z] = data[y - 1, z-1]
 
 print()
 
